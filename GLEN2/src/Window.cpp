72d097fdb0c5b751f4804ad35fb5b761ba307453
aljo242 <48221223+aljo242@users.noreply.github.com>
2020-05-14 16:07:21 -0400

    std container triangles

--- GLEN2/src/Window.cpp
+++ GLEN2/src/Window.cpp
@@ -11,11 +11,18 @@ const char *vertexShaderSource {"#version 460 core\n"
 	"}\0"};
 
 
-const char *fragmentShaderSource {"#version 460 core\n"
+const char *fragmentShaderSource1 {"#version 460 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
 "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0" }; 
+
+const char *fragmentShaderSource2 {"#version 460 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(1.0f, 0.1f, 0.7f, 0.5f);\n"
 "}\0" }; 
 
 Window::Window(const std::string name, const int width, const int height)
@@ -68,25 +75,32 @@ void Window::DoFrame()
 		GLEN_ERROR(infoLog);
 	}
 
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	glGetProgramiv(fragmentShader, GL_LINK_STATUS, &success);
-	if (!success)
+	std::vector<unsigned int> fragmentShaders(2);
+	int counter {0};
+	for (auto& shader : fragmentShaders)
 	{
-		glGetProgramInfoLog(fragmentShader, logSize, NULL, infoLog);
-		GLEN_ERROR("ERROR::SHADER::FRAGMENT::COMPILATION::FAILED\n");
-		GLEN_ERROR(infoLog);
-	}
+		shader = glCreateShader(GL_FRAGMENT_SHADER);
+		
+		glShaderSource(shader, 1, &fragmentShaderSource2, NULL);
+		if (counter == 1)
+		{
+			glShaderSource(shader, 1, &fragmentShaderSource1, NULL);
+		}
+		glCompileShader(shader);
 
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shader, logSize, NULL, infoLog);
+			GLEN_ERROR("ERROR::SHADER::FRAGMENT::COMPILATION::FAILED\n");
+			GLEN_ERROR(infoLog);
+		}
+	}
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
 
 	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
+	glAttachShader(shaderProgram, fragmentShaders[1]);
 	glLinkProgram(shaderProgram);
 
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
@@ -98,39 +112,52 @@ void Window::DoFrame()
 	}
 
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);  
+	glDeleteShader(fragmentShaders[1]);  
+
+	    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    std::vector<float> triangle  {
+        -0.9f, -0.5f, 0.0f,  // left 
+        -0.0f, -0.5f, 0.0f,  // right
+        -0.45f, 0.5f, 0.0f,  // top 
+
+         0.0f, -0.5f, 0.0f,  // left
+         0.9f, -0.5f, 0.0f,  // right
+         0.45f, 0.5f, 0.0f   // top 
+    };
 
 	// set up vertices
 	// refactor to std::vector<float>
-	float vertices[] {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
-	};
+	/*
+    float vertices[] = {
+         0.9f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.9f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+	*/
+	std::vector<unsigned int> VBOs(2);
+	std::vector<unsigned int> VAOs(2);
+	constexpr int triangleSize {9};
 
+	glGenVertexArrays(VAOs.size(), VAOs.data());
+	glGenBuffers(VBOs.size(), VBOs.data());
 
-	unsigned int VBO {0};
-	glGenBuffers(1, &VBO);
-
-	unsigned int VAO {0};
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	// copies the data
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	//
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// you can unbidn the VAO afterwards so other VAO calls won't accidentaly modifyy VAO
-	glBindVertexArray(0);
+	for (int i {0}; i < VBOs.size(); ++i)
+	{
+		glBindVertexArray(VAOs[i]);
+		glBindBuffer(GL_ARRAY_BUFFER, VBOs[i]);
+		glBufferData(GL_ARRAY_BUFFER, triangleSize * sizeof(float), triangle.data() + i*triangleSize , GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+	}
 
 	// wireframe render
-	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// rendering loop
 	while(!glfwWindowShouldClose(window))
@@ -142,16 +169,24 @@ void Window::DoFrame()
 
 		// DRAW 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO); // re-bind
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		for (int i {0}; i < VBOs.size(); ++i)
+		{
+			glBindVertexArray(VAOs[i]);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+		}
+
+		
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
 
 	// de-allocate all resources when done
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VAO);
+	glDeleteVertexArrays(2, VAOs.data());
+	glDeleteBuffers(1, VBOs.data());
+	//glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 }
 
